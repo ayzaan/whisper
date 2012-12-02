a28f94d40cac26893ddf1c97a9a3bbc9908cb0fe
Vinit Nayak <vinitnayak87@gmail.com>
2012-12-01 17:01:34 -0800

    js bug present on form submit.

---
+++ extension/submitform.js
@@ -0,0 +1,12 @@
+debugger;
+function what() {
+  // document.myform.submit();
+  alert("yo");
+  document.write("hsh");
+  console.log("WTF");
+  simpleform.username.value = "foo";
+}
+window.onload = function(){
+
+	debugger;
+}
