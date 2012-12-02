22ee89905662441fdbc6d2299064e40eb94831ce
bksim <simpavid@gmail.com>
2012-12-02 03:56:13 -0500

    contentscript

--- extension/contextmenu.js
+++ extension/contextmenu.js
@@ -1,7 +1,8 @@
 function firebasesucks(info)
 {
+	alert(info[0]);
  //do stuff here
- chrome.tabs.create({url: "https://www.google.com/search?q=firebase+sucks%3F&oq=firebase+sucks%3F&aqs=chrome.0.57.1610&sugexp=chrome,mod=1&sourceid=chrome&ie=UTF-8"})
+ //chrome.tabs.create({url: "https://www.google.com/search?q=firebase+sucks%3F&oq=firebase+sucks%3F&aqs=chrome.0.57.1610&sugexp=chrome,mod=1&sourceid=chrome&ie=UTF-8"})
 }
 
 function attach(info)
@@ -14,3 +15,10 @@ function attach(info)
 chrome.contextMenus.create ({
     title:"Encrypt with Whisper", contexts:["editable"], onclick:firebasesucks
 });
+
+chrome.tabs.sendRequest(tab.id, 'getSelection', sendRequestCallbackHandler);
+
+function sendRequestCallbackHandler(response) {
+   // response contains the selected text
+   console.log(String(response));
+}
