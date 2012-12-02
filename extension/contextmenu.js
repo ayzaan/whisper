48edd511d926d66719fcaf88f4c861057aa2ab1d
Omar Ramadan <omar.ramadan@berkeley.edu>
2012-12-02 02:42:44 -0800

    Working auto attach

--- extension/contextmenu.js
+++ extension/contextmenu.js
@@ -4,22 +4,16 @@ function firebasesucks(info)
  chrome.tabs.create({url: "https://www.google.com/search?q=firebase+sucks%3F&oq=firebase+sucks%3F&aqs=chrome.0.57.1610&sugexp=chrome,mod=1&sourceid=chrome&ie=UTF-8"})
 }
 
-function encrypt(field){
-	alert(field.value);
-	field.value = 'This is an encrypted message'
-}
+
 
 function attach(e)
-{
-	if (this.form != null){
-		alert('we have a form')
-		this.form.addEventListener('submit', function(e){ encrypt(this) }, true);
-	} else {
-		this.addEventListener('keypress', function(e){ alert('keypress'); if (e.keyCode == 13 && !e.shiftKey) { encrypt(this) } }, true);
-	}
-	alert('Attached' + this.value);
+{	
+	chrome.tabs.getSelected(null, function(tab) {
+	  chrome.tabs.sendMessage(tab.id, {method: "enableEncrypt"}, null);
+	});
+	
 }
 
 chrome.contextMenus.create ({
-    title:"Encrypt with Whisper", contexts:["editable"], onclick:attach
+	title:"Encrypt with Whisper", contexts:["editable"], onclick:attach
 });
