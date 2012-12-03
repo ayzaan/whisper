c167bfa8092f995fd2572bd06a228e7d60f1a0d7
Vinit Nayak <vinitnayak87@gmail.com>
2012-12-02 23:56:51 -0800

    Dynamic context menus. Does NOT currently work!

--- extension/js/contextmenu.js
+++ extension/js/contextmenu.js
@@ -6,6 +6,23 @@ function attach(e)
 	
 }
 
-chrome.contextMenus.create ({
-	title:"Encrypt with Whisper", contexts:["editable"], onclick:attach
-});
+function setMenu(groups) {
+	var differentGroups = groups.split();
+	for (var i = differentGroups.length - 1; i >= 0; i--) {
+		chrome.contextMenus.create ({
+			title:differentGroups[i], contexts:["editable"], onclick:attach,
+		});
+	};
+}
+
+// chrome.contextMenus.create ({
+// 	title:"Encrypt with Whisper", contexts:["editable"], onclick:attach,
+// });
+
+// chrome.contextMenus.create ({
+// 	title:"Option 1", contexts:["editable"], onclick:attach,
+// });
+
+// chrome.contextMenus.create ({
+// 	title:"Option 2", contexts:["editable"], onclick:attach, 
+// });
